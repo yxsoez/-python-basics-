74e3aaf65c7b5771e967c1f6608fcedd5f0540f9
yxsoez <elmooriginal123@gmail.com>
2026-04-15 13:18:06 -0500

    Add files via upload

---
+++ Primer programa.py
@@ -0,0 +1,5 @@
+# Es la primera vez que utilizo este programa
+# aprendí mi primer código print
+# para mostrar el dato en la consola
+
+print("hello world!")
---
+++ Variables.py
@@ -0,0 +1,13 @@
+# --- Operaciones Básicas con Variables ---
+# Autor: yxsoez
+# Descripción: Ejercicio simple para entender la declaración y multiplicación de variables.
+
+x = 20
+y = 100
+
+# Calculamos el resultado
+resultado = x * y
+
+# Mostramos el resultado de forma clara
+print(f"El resultado de multiplicar {x} por {y} es: {resultado}")
+
